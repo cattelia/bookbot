2172afa3e9fea27565a2f3106c353abfdf1dff74
cattelia <cattelia@gmail.com>
2024-07-03 10:20:26 -0700

    Adding demo gif and terminal command output.

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+'''
+ __          __         _                                           _           
+ \ \        / /        | |                                         | |          
+  \ \  /\  / /    ___  | |   ___    ___    _ __ ___     ___        | |_    ___  
+   \ \/  \/ /    / _ \ | |  / __|  / _ \  | '_ ` _ \   / _ \       | __|  / _ \ 
+    \  /\  /    |  __/ | | | (__  | (_) | | | | | | | |  __/       | |_  | (_) |
+     \/  \/      \___| |_|  \___|  \___/  |_| |_| |_|  \___|        \__|  \___/ 
+                                                                               
+                         __   __   __          __   __  ___ 
+                        |__) /  \ /  \ |__/   |__) /  \  |  
+                        |__) \__/ \__/ |  \   |__) \__/  |  
+                                 
+'''
+import time
+# For terminal output.
+string = " __          __         _                                           _           \n \ \        / /        | |                                         | |          \n  \ \  /\  / /    ___  | |   ___    ___    _ __ ___     ___        | |_    ___  \n   \ \/  \/ /    / _ \ | |  / __|  / _ \  | '_ ` _ \   / _ \       | __|  / _ \ \n    \  /\  /    |  __/ | | | (__  | (_) | | | | | | | |  __/       | |_  | (_) |\n     \/  \/      \___| |_|  \___|  \___/  |_| |_| |_|  \___|        \__|  \___/ \n\n                         __   __   __          __   __  ___ \n                        |__) /  \ /  \ |__/   |__) /  \  |  \n                        |__) \__/ \__/ |  \   |__) \__/  |  \n\n\n"
+print(string)
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
@@ -5,8 +23,12 @@ def main():
     chars_dict = get_chars_dict(text)
     #print(chars_dict)
     print(f"--- Begin report of {book_path} ---")
+    time.sleep(2)
+    print(f"--- Grabbing contents ---\n")
+    time.sleep(2)
     print(f"{num_words} words found in the documents \n")
     get_report(chars_dict)
+    time.sleep(1)
     print("--- End report ---")
 
 
